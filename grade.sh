dd3a519c61a3ebf8385d681d1ae9650c236b713d
JustCode65 <justkim1128@gmail.com>
2023-11-27 12:32:43 -0800

    help
    help

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
 CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
+CPATH2='.;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -19,7 +20,7 @@ fi
 
 javac -cp $CPATH *.java
 
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+java -cp $CPATH2 org.junit.runner.JUnitCore TestListExamples > junit-output.txt
 
 FAILURES=`grep -c FAILURES!!! junit-output.txt`
 ERROR=`grep -c error junit-output.txt`
